cbd847878c2a807d5414bdae32fef06146f831c3
nadoo <287492+nadoo@users.noreply.github.com>
2018-07-18 00:06:19 +0800

    add codes

---
+++ help.go
@@ -0,0 +1,49 @@
+// ALL possible goos and goarch
+// https://github.com/golang/go/blob/master/src/go/build/syslist.go
+// const goosList = "android darwin dragonfly freebsd linux nacl netbsd openbsd plan9 solaris windows zos "
+// const goarchList = "386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc s390 s390x sparc sparc64 "
+
+// for arm:
+// https://github.com/golang/go/wiki/GoArm
+//
+
+package main
+
+import "fmt"
+
+func showHelp() {
+	fmt.Println()
+	fmt.Println("-")
+	fmt.Println("Usage: goc COMMAND [ARGS]")
+	fmt.Println()
+	fmt.Println("COMMAND")
+	fmt.Println("     b: \tbuild package")
+	fmt.Println("     bw: \tbuild windows package")
+	fmt.Println("     bwp: \tbuild windows pprof package(-tags=pprof)")
+	fmt.Println("     bl: \tbuild linux package")
+	fmt.Println("     blp: \tbuild linux pprof package(-tags=pprof)")
+	fmt.Println("     bm: \tbuild mac package")
+	fmt.Println("     r: \trelease package(build without debug info)")
+	fmt.Println("     rw: \trelease windows package")
+	fmt.Println("     rw32: \trelease windows package(x86)")
+	fmt.Println("     rl: \trelease linux package")
+	fmt.Println("     rl32: \trelease linux package(x86)")
+	fmt.Println("     rla: \trelease linux package(arm)")
+	fmt.Println("     rla5: \trelease linux package(arm v5)")
+	fmt.Println("     rla6: \trelease linux package(arm v6)")
+	fmt.Println("     rla7: \trelease linux package(arm v7)")
+	fmt.Println("     rla8: \trelease linux package(arm v8)")
+	fmt.Println("     rlm: \trelease linux package(mips)")
+	fmt.Println("     rm: \trelease mac package")
+	fmt.Println("     rm32: \trelease mac package(x86)")
+	fmt.Println()
+	fmt.Println("Example:")
+	fmt.Println("     goc b")
+	fmt.Println("       -go build in current directory")
+	fmt.Println("     goc rl")
+	fmt.Println("       -release linux package")
+	fmt.Println()
+	fmt.Println("   Ver: 1.0, Author: NadOo")
+	fmt.Println("   Source: https://github.com/nadoo/goc")
+	fmt.Println("-")
+}
---
+++ main.go
@@ -0,0 +1,111 @@
+// ALL possible goos and goarch
+// https://github.com/golang/go/blob/master/src/go/build/syslist.go
+// const goosList = "android darwin dragonfly freebsd linux nacl netbsd openbsd plan9 solaris windows zos "
+// const goarchList = "386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc s390 s390x sparc sparc64 "
+
+// for arm:
+// https://github.com/golang/go/wiki/GoArm
+//
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+// GO Environment
+var (
+	GOROOT = os.Getenv("GOROOT")
+	GOPATH = os.Getenv("GOPATH")
+)
+
+type build struct {
+	cmd  string
+	os   string
+	arch string
+	arm  string
+	dir  string
+	args []string
+}
+
+var builds = []build{
+	{"b", runtime.GOOS, runtime.GOARCH, "", "", []string{"build", "-v", "-i"}},
+	{"bw", "windows", "amd64", "", "", []string{"build", "-v", "-i"}},
+	{"bwp", "windows", "amd64", "", "", []string{"build", "-v", "-i", "-tags=pprof"}},
+	{"bl", "linux", "amd64", "", "", []string{"build", "-v", "-i"}},
+	{"blp", "linux", "amd64", "", "", []string{"build", "-v", "-i", "-tags=pprof"}},
+	{"bm", "darwin", "amd64", "", "", []string{"build", "-v", "-i"}},
+
+	{"r", runtime.GOOS, runtime.GOARCH, "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rw", "windows", "amd64", "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rw32", "windows", "386", "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rl", "linux", "amd64", "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rl32", "linux", "386", "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rla", "linux", "arm", "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rla5", "linux", "arm", "5", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rla6", "linux", "arm", "6", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rla7", "linux", "arm", "7", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rla8", "linux", "arm", "8", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rlm", "linux", "mips", "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rlmle", "linux", "mipsle", "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rm", "darwin", "amd64", "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+	{"rm32", "darwin", "386", "", "", []string{"build", "-v", "-i", "-ldflags", "-s -w"}},
+}
+
+// Build :
+func Build(b build, args ...string) {
+	cmd := exec.Command("go", args...)
+	cmd.Env = os.Environ()
+	cmd.Dir = b.dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Env = append(cmd.Env, "GOARCH="+b.arch)
+	cmd.Env = append(cmd.Env, "GOOS="+b.os)
+
+	if b.arm != "" {
+		cmd.Env = append(cmd.Env, "GOARM="+b.arm)
+	}
+
+	fmt.Println("-Environments:")
+	fmt.Println("   GOROOT:" + GOROOT)
+	fmt.Println("   GOPATH:" + GOPATH)
+	fmt.Println("   GOARCH:" + b.arch)
+	fmt.Println("   GOOS:" + b.os)
+
+	fmt.Println("-Working Dir:")
+	fmt.Printf("   %s\n", cmd.Dir)
+
+	fmt.Println("-Exec program:")
+	fmt.Printf("   path: %s\n", cmd.Path)
+	fmt.Printf("   args: %v\n\n", cmd.Args[1:])
+
+	fmt.Println("\n-Exec outputs:\n-----")
+
+	if err := cmd.Run(); err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+	}
+}
+
+func main() {
+	argc := len(os.Args)
+	if argc < 2 {
+		showHelp()
+		return
+	}
+
+	cmd := os.Args[1]
+	dir, _ := os.Getwd()
+
+	for _, b := range builds {
+		if b.cmd == cmd {
+			b.dir = dir
+			b.args = append(b.args, os.Args[2:]...)
+			Build(b, b.args...)
+			break
+		}
+	}
+}
