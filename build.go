52ef755ab954b5949b5d8f056f15cd78705fcbb0
nadoo <287492+nadoo@users.noreply.github.com>
2022-04-19 22:53:26 +0800

    support GOAMD64

--- build.go
+++ build.go
@@ -11,46 +11,52 @@ type build struct {
 	cmd  string
 	os   string
 	arch string
-	arm  string
+	ver  string // version of arm or amd64
 	dir  string
 	args []string
+	help string
 }
 
 var builds = []build{
-	{"b", runtime.GOOS, runtime.GOARCH, "", "", []string{"build", "-v"}},
-	{"bd", runtime.GOOS, runtime.GOARCH, "", "", []string{"build", "-v", "-tags=dev"}},
-	{"bdr", runtime.GOOS, runtime.GOARCH, "", "", []string{"build", "-v", "-tags=dev", "-race"}},
-	{"bw", "windows", "amd64", "", "", []string{"build", "-v"}},
-	{"bwd", "windows", "amd64", "", "", []string{"build", "-v", "-tags=dev"}},
-	{"bwdr", "windows", "amd64", "", "", []string{"build", "-v", "-tags=dev", "-race"}},
-	{"bl", "linux", "amd64", "", "", []string{"build", "-v"}},
-	{"bld", "linux", "amd64", "", "", []string{"build", "-v", "-tags=dev"}},
-	{"bldr", "linux", "amd64", "", "", []string{"build", "-v", "-tags=dev", "-race"}},
-	{"blad", "linux", "arm64", "", "", []string{"build", "-v", "-tags=dev"}},           // arm64 == arm v8
-	{"bladr", "linux", "arm64", "", "", []string{"build", "-v", "-tags=dev", "-race"}}, // arm64 == arm v8
-	{"bm", "darwin", "amd64", "", "", []string{"build", "-v"}},
-	{"bmd", "darwin", "amd64", "", "", []string{"build", "-v", "-tags=dev"}},
-	{"bmdr", "darwin", "amd64", "", "", []string{"build", "-v", "-tags=dev", "-race"}},
-	{"bma", "darwin", "arm64", "", "", []string{"build", "-v"}},
-	{"bmad", "darwin", "arm64", "", "", []string{"build", "-v", "-tags=dev"}},
-	{"bmadr", "darwin", "arm64", "", "", []string{"build", "-v", "-tags=dev", "-race"}},
+	{"b", runtime.GOOS, runtime.GOARCH, "", "", []string{"build", "-v"}, "build package"},
+	{"bd", runtime.GOOS, runtime.GOARCH, "", "", []string{"build", "-v", "-tags=dev"}, "build dev package(-tags=dev)"},
+	{"bdr", runtime.GOOS, runtime.GOARCH, "", "", []string{"build", "-v", "-tags=dev", "-race"}, "build dev package(-tags=dev and -race)"},
+	{"bw", "windows", "amd64", "", "", []string{"build", "-v"}, "build windows package"},
+	{"bwd", "windows", "amd64", "", "", []string{"build", "-v", "-tags=dev"}, "build windows dev package(-tags=dev)"},
+	{"bwdr", "windows", "amd64", "", "", []string{"build", "-v", "-tags=dev", "-race"}, "build windows dev package(-tags=dev and -race)"},
+	{"bl", "linux", "amd64", "", "", []string{"build", "-v"}, "build linux package"},
+	{"bl3", "linux", "amd64", "v3", "", []string{"build", "-v"}, "build linux package(amdv3)"},
+	{"bld", "linux", "amd64", "", "", []string{"build", "-v", "-tags=dev"}, "build linux dev package(-tags=dev)"},
+	{"bld3", "linux", "amd64", "v3", "", []string{"build", "-v", "-tags=dev"}, "build linux dev package(-tags=dev and amdv3)"},
+	{"bldr", "linux", "amd64", "", "", []string{"build", "-v", "-tags=dev", "-race"}, "build linux dev package(-tags=dev and -race)"},
+	{"blad", "linux", "arm64", "", "", []string{"build", "-v", "-tags=dev"}, "build linux dev package(-tags=dev)(arm64/arm v8)"},                     // arm64 == arm v8
+	{"bladr", "linux", "arm64", "", "", []string{"build", "-v", "-tags=dev", "-race"}, "build linux dev package(-tags=dev and -race)(arm64/arm v8)"}, // arm64 == arm v8
+	{"bm", "darwin", "amd64", "", "", []string{"build", "-v"}, "build mac package"},
+	{"bmd", "darwin", "amd64", "", "", []string{"build", "-v", "-tags=dev"}, "build mac dev package(-tags=dev)"},
+	{"bmdr", "darwin", "amd64", "", "", []string{"build", "-v", "-tags=dev", "-race"}, "build mac dev package(-tags=dev and -race)"},
+	{"bma", "darwin", "arm64", "", "", []string{"build", "-v"}, "build mac package(arm64)"},
+	{"bmad", "darwin", "arm64", "", "", []string{"build", "-v", "-tags=dev"}, "build mac dev package(-tags=dev)(arm64)"},
+	{"bmadr", "darwin", "arm64", "", "", []string{"build", "-v", "-tags=dev", "-race"}, "build mac dev package(-tags=dev and -race)(arm64)"},
 
-	{"rw", "windows", "amd64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rw32", "windows", "386", "", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rl", "linux", "amd64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rl32", "linux", "386", "", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rla", "linux", "arm64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}}, // arm64 == arm v8
-	{"rla5", "linux", "arm", "5", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rla6", "linux", "arm", "6", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rla7", "linux", "arm", "7", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rlm", "linux", "mips", "", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rlmle", "linux", "mipsle", "", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rm", "darwin", "amd64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}},
-	{"rma", "darwin", "arm64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}},
+	{"rw", "windows", "amd64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release windows package"},
+	{"rw3", "windows", "amd64", "v3", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release windows package(amd64v3)"},
+	{"rw32", "windows", "386", "", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release windows package(x86)"},
+	{"rl", "linux", "amd64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release linux package"},
+	{"rl3", "linux", "amd64", "v3", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release linux package(amd64v3)"},
+	{"rl32", "linux", "386", "", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release linux package(x86)"},
+	{"rla", "linux", "arm64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release linux package(arm64/arm v8)"}, // arm64 == arm v8
+	{"rla5", "linux", "arm", "5", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release linux package(arm v5)"},
+	{"rla6", "linux", "arm", "6", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release linux package(arm v6)"},
+	{"rla7", "linux", "arm", "7", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release linux package(arm v7)"},
+	{"rlm", "linux", "mips", "", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release linux package(mips)"},
+	{"rlmle", "linux", "mipsle", "", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release linux package(mipsle)"},
+	{"rm", "darwin", "amd64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release mac package"},
+	{"rm3", "darwin", "amd64", "v3", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release mac package(amd64v3)"},
+	{"rma", "darwin", "arm64", "", "", []string{"build", "-v", "-ldflags", "-s -w"}, "release mac package(arm64)"},
 
-	{"r", runtime.GOOS, runtime.GOARCH, "", "", []string{"run", "."}},
-	{"i", runtime.GOOS, runtime.GOARCH, "", "", []string{"install"}},
-	{"c", runtime.GOOS, runtime.GOARCH, "", "", []string{"clean"}},
+	{"r", runtime.GOOS, runtime.GOARCH, "", "", []string{"run", "."}, "run current package"},
+	{"i", runtime.GOOS, runtime.GOARCH, "", "", []string{"install"}, "install package to `GOBIN` or `GOPATH/bin`"},
+	{"c", runtime.GOOS, runtime.GOARCH, "", "", []string{"clean"}, "clean package"},
 }
 
 // Build is used to execute build commands.
@@ -78,9 +84,15 @@ func Build(b build, args ...string) {
 	fmt.Println("   GOOS:\t" + b.os)
 	fmt.Println("   GOARCH:\t" + b.arch)
 
-	if b.arm != "" {
-		cmd.Env = append(cmd.Env, "GOARM="+b.arm)
-		fmt.Println("   GOARM:\t" + b.arm)
+	if b.ver != "" {
+		switch b.arch {
+		case "arm":
+			cmd.Env = append(cmd.Env, "GOARM="+b.ver)
+			fmt.Println("   GOARM:\t" + b.ver)
+		case "amd64":
+			cmd.Env = append(cmd.Env, "GOAMD64="+b.ver)
+			fmt.Println("   GOAMD64:\t" + b.ver)
+		}
 	}
 
 	fmt.Println("-Working Dir:")
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 // ALL possible goos and goarch
+// command: go tool dist list
 // https://github.com/golang/go/blob/master/src/go/build/syslist.go
 // const goosList = "aix android darwin dragonfly freebsd hurd illumos js linux nacl netbsd openbsd plan9 solaris windows zos "
 // const goarchList = "386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc riscv riscv64 s390 s390x sparc sparc64 wasm "
 
 // for arm:
 // https://github.com/golang/go/wiki/GoArm
-//
+
+// for amd64
+// https://github.com/golang/go/wiki/MinimumRequirements#amd64
 
 package main
 
